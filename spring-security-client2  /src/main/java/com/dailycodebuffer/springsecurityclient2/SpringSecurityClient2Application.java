1fb5f43c1dbf496046632fc0982aa9b0a7381329
220779 <102145019+220779@users.noreply.github.com>
2023-12-11 14:08:48 +0200

    Initial commit

---
+++ spring-security-client2  /src/main/java/com/dailycodebuffer/springsecurityclient2/SpringSecurityClient2Application.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springsecurityclient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityClient2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityClient2Application.class, args);
+	}
+
+}
---
+++ spring-security-client2  /src/test/java/com/dailycodebuffer/springsecurityclient2/SpringSecurityClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springsecurityclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityClient2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
