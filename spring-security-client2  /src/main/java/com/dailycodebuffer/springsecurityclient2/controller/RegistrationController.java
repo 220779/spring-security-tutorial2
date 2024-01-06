b8091fa3fad0769f6e469f4a05a497479f5830bd
220779 <102145019+220779@users.noreply.github.com>
2024-01-06 11:03:54 +0200

    [add] Listening to multiple events

--- spring-security-client2  /src/main/java/com/dailycodebuffer/springsecurityclient2/controller/RegistrationController.java
+++ spring-security-client2  /src/main/java/com/dailycodebuffer/springsecurityclient2/controller/RegistrationController.java
@@ -4,7 +4,8 @@ import com.dailycodebuffer.springsecurityclient2.entity.User;
 import com.dailycodebuffer.springsecurityclient2.model.UserModel;
 import com.dailycodebuffer.springsecurityclient2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,8 @@ public class RegistrationController {
 
     @Autowired
     private UserService userService;
+
+    private ApplicationEventPublisher publisher;
 
     @PostMapping("/register")
     public String registerUser (@RequestBody UserModel userModel) {
